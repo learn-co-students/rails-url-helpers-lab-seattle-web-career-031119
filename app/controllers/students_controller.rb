462223709eaba52eb6c5dea371413425a3d2dd92
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-05 13:38:22 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
-  def show
+  def activate
+    @student.active ? @student.active = false : @student.active = true
+    @student.save
+    redirect_to @student
+    # ^ crazy quick way
+    #redirect_to student_path(@student) - good rails way
+    #redirect_to student_path(@student.id) - other rails way
+    #redirect_to "/students/#{@student.id} - sinatra way
   end
 
   private

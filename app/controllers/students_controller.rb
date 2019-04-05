d481102ad6f8c74de76068fc0e6cc84a81f51ca0
quinncidences <qrc.inpa@gmail.com>
2019-04-05 13:43:52 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    true == @student.active ? @student.active = false : @student.active = true
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
